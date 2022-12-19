7d5731017ef87e18074f09f1ac15199a8f685679
Ryan Dong <ryandong@Ryans-MacBook-Pro-2.local>
2022-12-18 19:58:06 -0800

    added db models

--- 02-Challenge/Develop/models/Product.js
+++ 02-Challenge/Develop/models/Product.js
@@ -34,7 +34,10 @@ Product.init(
         isInt: true,
       },
     },
-    
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: { model: "category", key: "id" },
+    } 
   },
   {
     sequelize,
