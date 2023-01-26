f854bfdcb5ca527075d1061d423ebd7a9540d8ab
Ryan Dong <ryandong@Ryans-MacBook-Pro-2.local>
2023-01-25 23:46:59 -0800

    added tag routes

--- 02-Challenge/Develop/routes/api/tag-routes.js
+++ 02-Challenge/Develop/routes/api/tag-routes.js
@@ -6,11 +6,46 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-});
+  tag.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      },
+    ],
+  })
+    .then((tag) => {
+      return res.status(200).json(tag);
+    }
+    )
+    .catch((err) => {
+      return res.status(500).send(err);
+    }
+    );
+    });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  tag.findByPk(req.params.id, {
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      },
+    ],
+  })
+    .then((tag) => {
+      if (!tag) {
+        return res.status(404).json({ message: "Tag not found" });
+      }
+      return res.status(200).json(tag);
+    }
+    )
+    .catch((err) => {
+      res.status(400).json(err);
+    }
+    );
 });
 
 router.post('/', (req, res) => {
